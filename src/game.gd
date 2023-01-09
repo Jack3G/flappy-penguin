db8b528220d1bd078a67d313d1c04d2d6ab14c05
Jack3G <40290244+Jack3G@users.noreply.github.com>
2023-01-09 17:11:51 +1100

    add a menu button

--- src/game.gd
+++ src/game.gd
@@ -148,6 +148,11 @@ func _ready() -> void:
 	respawn()
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("menu"):
+		get_tree().change_scene_to_file("res://src/main_menu.tscn")
+
+
 func _physics_process(delta: float) -> void:
 	if continue_moving:
 		distance += scroll_speed * delta
